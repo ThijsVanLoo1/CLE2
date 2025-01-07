e121318f91ee958895f4ca7daddfc8353d471312
RexTheWeeb <D.F.vantwoudt@outlook.com>
2025-01-07 13:49:49 +0100

    Tailwind

--- CLE2/index.php
+++ CLE2/index.php
@@ -1,14 +1,30 @@
-<!doctype html>
+<?php
+
+
+?> <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./output.css" rel="stylesheet">
     <title>Document</title>
     <header>Hallo</header>
 </head>
 <body>
-
+<ul class="days">
+    <li>Ma</li>
+    <li>Di</li>
+    <li>Wo</li>
+    <li>Do</li>
+    <li>Vr</li>
+</ul>
+<ul class="flex">
+    <li>18:00</li>
+    <li>18:30</li>
+    <li>19:00</li>
+    <li>19:30</li>
+</ul>
 </body>
 </html>
